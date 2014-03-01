2bf19a8672349231c9b21e300a7909f06de7f56d
mie00 <melawadi2005@yahoo.com>
2014-03-01 17:29:46 +0200

    Initial commit
    Good start but can be better

---
+++ app.js
@@ -0,0 +1,353 @@
+function $$(id){return document.getElementById(id);}
+function eq(a,b){
+    if (a.length!=b.length)return false
+    for (var i = 0; i < a.length; i++) {
+        if (a[i]!=b[i])return false
+    };
+
+    return true
+}
+function Element(tag,classes,parent,content,args,undef){
+    self=this;
+	if (content=="lorem")content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, officia, laudantium, iusto minus similique aliquam sapiente deserunt qui illo quia quam magni voluptatem atque maiores cum eveniet accusantium impedit quae cupiditate ea amet rem sed mollitia quis delectus libero sequi adipisci nobis hic animi voluptates rerum maxime dolorum. Voluptate, animi minus totam delectus aliquam hic itaque dolorum exercitationem eum obcaecati odio rerum error repellendus labore suscipit ipsum deserunt at dicta doloribus atque provident sapiente ut tempora dolore et! Magnam sint dicta a commodi consectetur. Consequatur, odit, veniam, minus nobis deleniti ducimus harum praesentium sed accusantium quam voluptatum molestiae sapiente eveniet."
+    this.tag = (tag=="|")?"":tag;
+    if (match1=classes.match(/\.\w+/g)){
+        this.classes = match1.join(' ');
+        this.classes=this.classes.replace(/\./g,'')
+    }
+    if (match1=classes.match(/#(\w+)/)){
+        this.id = match1[1];
+    }
+    this.content = content;
+    var args2={};
+    if (args)args.slice(1,-1).split(',').forEach(function(x){var z=x.split('=');args2[z[0]]=(z[1]!=undef)?z[1]:null})
+    this.args=args2;
+    this.parent = parent;
+    this.children = [];
+    this.render=function(){
+        var args2=this.args
+        if (this.tag){
+            result="<"+this.tag+((this.id)?" id='"+this.id+"'":"")+((this.classes)?" class='"+this.classes+"'":"")+
+            
+            ((args2)?Object.keys(args2).reduce(function(x,y){return x+" "+y+((args2[y]!=null)?('='+args2[y]):"")},""):"")
+
+            +">"+this.content;
+            for (var i = 0; i < this.children.length; i++) {
+                result+=this.children[i].render()
+            };
+            result+="</"+this.tag+">";
+        }
+        else{
+            result=this.content;
+        }
+        return result
+
+    };
+}
+function parse(jade){
+    jade=jade.replace(/^\t/gm,"    ")
+    jade=jade.replace(/</g,'&lt;').replace(/>/g,'&gt;')
+    sep='\n'
+    if (jade.indexOf('\n\r')>=0)sep='\n\r';
+    else if (jade.indexOf('\r')>=0)sep='\r';
+
+    str=jade.split(sep)
+    ladd=[];
+    ladd[-1]=new Element("div","#appcontainer","","","");
+    isnormaltext=0
+
+
+    for (var i = 0; i < str.length; i++) {
+        sent=str[i]
+        if (sent.match(/^\s*$/))continue;
+        if (isnormaltext){
+
+            match=sent.match(new RegExp("^[ ]{"+(isnormaltext)+"}(.*)$"));
+            if (match!=null){
+                element=new Element("|","",ladd[isnormaltext/4-1],match[1]+'\n');
+                ladd[isnormaltext/4-1].children.push(element)
+            
+            }
+            else{
+                isnormaltext=0;
+            }
+
+        }
+        if (!isnormaltext){
+
+            match=sent.match(/^((?:[ ]{4})*)(\w+|\|)((?:\.\w+)*(?:#\w+)?(?:\.\w+)*)(\.?)((?:\([^)]+\))?)[ ]*(.*)$/);
+            console.log(match)
+            element=new Element(match[2],match[3],ladd[match[1].length/4-1],match[6],match[5]);
+            if(ladd[match[1].length/4-1])ladd[match[1].length/4-1].children.push(element)
+            ladd[match[1].length/4]=element;
+
+            if(match[4])isnormaltext=match[1].length+4;
+        }
+
+    };
+    return ladd[-1].render()
+}
+function refreshStyle(){
+    editor.save();
+	if($$('less-error-message:inline'))$$('less-error-message:inline').remove();
+	$$('miestyle').type='text/less';
+	$$('miestyle').innerHTML="#miecont2{"+$$('ts').value+"}";
+	less(window);
+}
+function refreshHTML(){
+    editor2.save();
+	$$('miebody').innerHTML=$$('tb').value=parse($$('ta').value);
+}
+function randfloat(x){
+    return Math.random()*x
+}
+function randint(x){
+    return Math.floor(randfloat(x+1))
+}
+function randchoise(arr){
+    return arr[randint(arr.length-1)]
+}
+function concfn(num,fn,args){
+    return Array.apply(null,Array(num)).map(function(){return randchoise.apply(null,args)})
+}
+function padzeros(str,num){
+    return Array.apply(null,Array(num-str.length)).map(function(){return '0'}).join("")+str;
+}
+var curr_dict=[];
+var curr_line;
+var curr_prop;
+var curr_id;
+var curr_undo=[]
+var curr_undo_loc;
+var val_dict={
+"alphavalue": ["0.7",function(){return randfloat(1)},function(str,ind,inc,step=0.1){if (!inc)step=-step;return Number(str)+step}],
+"non-negative-integer": ["2",function(){return randint(10)},function(str,ind,inc,step=1){if (!inc)step=-step;return Number(str)+step}],
+"number": ["2",function(){return randint(10)},function(str,ind,inc,step=1){if (!inc)step=-step;return Number(str)+step}],
+"integer": ["2",function(){return randint(10)},function(str,ind,inc,step=1){if (!inc)step=-step;return Number(str)+step}],
+"percentage": ["70%",function(){return randint(101)+'%'},function(str,ind,inc,step=1){if (!inc)step=-step;return Number(str.slice(0,-1))+step+'%'}],
+"angle": ["70deg",function(){return randint(360)+'deg'},function(str,ind,inc,step=10){if (!inc)step=-step;return Number(str.slice(0,-3))+step+'deg'}],
+//no change yet see color[2]
+"color": ["#ab65d0",function(){return '#'+padzeros(randint(16777215).toString(16),6)},function(str,ind,inc,step=1){return str}],
+"length": ["30px",function(){return randint(200)+'px'},function(str,ind,inc,step=1){if (!inc)step=-step;return Number(str.slice(0,-2))+step+'px'}],
+"string": ["'MIE'",function(){return "'"+concfn(6,randchoise,['0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ']).join('')+"'"},function(str,ind,inc,step=1){return str}],
+"time": ["2s",function(){return randint(5000)+'ms'},function(str,ind,inc,step=1){if (!inc)step=-step;return Number(str.slice(0,-1))+step+'S'}],
+"mime-type": ["image/gif",function(){return "image/gif"},function(str,ind,inc,step=1){return str}],
+"charset": ["UTF-8",function(){return "UTF-8"},function(str,ind,inc,step=1){return str}],
+"encoded-data": ["R0lGODlhEAAQAMQAAORHHOVSKudfOulrSOp3WOyDZu6QdvCchPGolfO0o/XBs/fNwfjZ0frl3/zy7////wAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACH5BAkAABAALAAAAAAQABAAAAVVICSOZGlCQAosJ6mu7fiyZeKqNKToQGDsM8hBADgUXoGAiqhSvp5QAnQKGIgUhwFUYLCVDFCrKUE1lBavAViFIDlTImbKC5Gm2hB0SlBCBMQiB0UjIQA7",function(){return "R0lGODlhEAAQAMQAAORHHOVSKudfOulrSOp3WOyDZu6QdvCchPGolfO0o/XBs/fNwfjZ0frl3/zy7////wAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACH5BAkAABAALAAAAAAQABAAAAVVICSOZGlCQAosJ6mu7fiyZeKqNKToQGDsM8hBADgUXoGAiqhSvp5QAnQKGIgUhwFUYLCVDFCrKUE1lBavAViFIDlTImbKC5Gm2hB0SlBCBMQiB0UjIQA7"},function(str,ind,inc,step=1){return str}],
+"x": ["30px",function(){return randint(200)+'px'},function(str,ind,inc,step=1){if (!inc)step=-step;return Number(str.slice(0,-2))+step+'px'}],
+"y": ["30px",function(){return randint(200)+'px'},function(str,ind,inc,step=1){if (!inc)step=-step;return Number(str.slice(0,-2))+step+'px'}]}
+
+function initialize(){
+    var cursor=editor.getCursor()
+    var line=cursor.line;
+    var l=editor.getLine(line);
+    if (match3=l.match(/^(\s*)([^:]+?)\s*:\s*(.*?)\s*;?$/)){
+        curr_prop=match3[1]+match3[2];
+        var prop=match3[2][0]!='-'?match3[2]:match3[2].match(/\-\w+\-(.+)$/)[1]
+        if (prop in css_dict){
+            curr_line=line
+            updateControls(parse1(css_dict[prop]))
+        }
+    }
+
+}
+function updateControls(dict){
+    var temp;
+    if (dict){
+        temp=dict.show()
+    }
+    else{
+        temp=curr_dict[0].show()
+    }
+    if(!eq(curr_dict,temp[1])){
+
+        curr_dict=temp[1];
+        $$("controls").innerHTML=temp[0];
+        document.getElementsByClassName("color1").map(function(el){new jscolor.color(el, 
+            {hash:true,pickerClosable:true,pickerMode:'HVS',
+                onImmediateChange:function(color){
+                    if(curr_dict[el.id.slice(4)].val!='#'+this){
+                        curr_dict[el.id.slice(4)].val='#'+this;
+                        updateControls();
+                    }
+                },adjust:false
+            }
+        )}
+        )
+
+        if (curr_id){
+            $$(curr_id).focus();
+            curr_id=null;
+        }
+    }
+    else{
+        console.log(123)
+        for (var i = 0; i < curr_dict.length; i++) {
+            $$("pla_"+i).value=curr_dict[i].render();
+        };
+    }
+    editor.setLine(curr_line,curr_prop+": "+curr_dict[0].render()+";")
+    refreshStyle()
+    
+
+}
+function pressed(ev,el){
+
+    e = ev || window.event;
+    var charCode = e.charCode || e.keyCode;
+    if (e.ctrlKey == true){
+        console.log(1)
+        if (charCode == 32){
+            curr_id=el.id;
+            e.preventDefault();
+            el.nextElementSibling.click()
+        }
+        if (charCode == 90 ||charCode == 122){
+            undo();
+            e.preventDefault();
+        }
+        if (charCode == 89 ||charCode == 121){
+            redo();
+            e.preventDefault();
+        }
+
+        if(el.previousElementSibling){
+            if (charCode == 39){
+                curr_id=el.id;
+                e.preventDefault();
+                el.nextElementSibling.nextElementSibling.click()
+
+            }
+            if (charCode == 37){
+                curr_id=el.id;
+                e.preventDefault();
+                el.previousElementSibling.click()
+
+            }
+        }
+    }
+     
+}
+function undo(){
+    if (curr_undo_loc-1>=0)
+        setProps(curr_undo_loc-1)
+}
+function redo(){
+    if (curr_undo_loc+1<curr_undo.length)
+        setProps(curr_undo_loc+1)
+}
+function setProps(i){
+    curr_undo_loc=i
+    var arr=curr_undo[i];
+    console.log(arr)
+    keys=Object.keys(arr)
+    for (var i = 0; i < keys.length; i++) {
+        curr_dict[keys[i]].val=arr[keys[i]];
+    };
+    updateControls();
+}
+function getProps(){
+    var arr={}
+    curr_undo_loc=curr_undo.length
+    for (var i = 0; i < curr_dict.length; i++) {
+        if (curr_dict[i].constructor==Variable){
+            arr[i]=$$("pla_"+i).value;
+        };
+    };
+    curr_undo.push(arr)
+}
+function resetProps(){
+    curr_undo=[];
+    curr_undo_loc=null;
+}
+/*
+TODO:
+changing less makes a bug
+less vars
+increase,decrease val
+spaces on tab before properties
+docs
+style
+option=range{0,1}
+*/
+
+
+/*!
+ * classie - class helper functions
+ * from bonzo https://github.com/ded/bonzo
+ * 
+ * classie.has( elem, 'my-class' ) -> true/false
+ * classie.add( elem, 'my-new-class' )
+ * classie.remove( elem, 'my-unwanted-class' )
+ * classie.toggle( elem, 'my-class' )
+ */
+
+/*jshint browser: true, strict: true, undef: true */
+/*global define: false */
+( function( window ) {
+
+
+
+// class helper functions from bonzo https://github.com/ded/bonzo
+
+function classReg( className ) {
+  return new RegExp("(^|\\s+)" + className + "(\\s+|$)");
+}
+
+// classList support for class management
+// altho to be fair, the api sucks because it won't accept multiple classes at once
+var hasClass, addClass, removeClass;
+
+if ( 'classList' in document.documentElement ) {
+  hasClass = function( elem, c ) {
+    return elem.classList.contains( c );
+  };
+  addClass = function( elem, c ) {
+    elem.classList.add( c );
+  };
+  removeClass = function( elem, c ) {
+    elem.classList.remove( c );
+  };
+}
+else {
+  hasClass = function( elem, c ) {
+    return classReg( c ).test( elem.className );
+  };
+  addClass = function( elem, c ) {
+    if ( !hasClass( elem, c ) ) {
+      elem.className = elem.className + ' ' + c;
+    }
+  };
+  removeClass = function( elem, c ) {
+    elem.className = elem.className.replace( classReg( c ), ' ' );
+  };
+}
+
+function toggleClass( elem, c ) {
+  var fn = hasClass( elem, c ) ? removeClass : addClass;
+  fn( elem, c );
+}
+
+var classie = {
+  // full names
+  hasClass: hasClass,
+  addClass: addClass,
+  removeClass: removeClass,
+  toggleClass: toggleClass,
+  // short names
+  has: hasClass,
+  add: addClass,
+  remove: removeClass,
+  toggle: toggleClass
+};
+
+// transport
+if ( typeof define === 'function' && define.amd ) {
+  // AMD
+  define( 'classie/classie',classie );
+} else {
+  // browser global
+  window.classie = classie;
+}
+
+})( window );
---
+++ struct.js
@@ -0,0 +1,315 @@
+function loop(num){
+	return Array.apply(null,Array(num));
+}
+function dictVal(dict){
+    return dict[dictKey(dict)];
+}
+function dictKey(dict){
+    return Object.keys(dict)[0];
+}
+function parse1(dict){
+	var key=dictKey(dict);
+	var val=dictVal(dict);
+	switch (key){
+		case 'or':
+			return new Choose(val.map(parse1));
+        case 'concat':
+        case 'orderand':
+			return new Concatinate(val.map(parse1));
+		case 'optional':
+			return new Check(parse1(val));
+		case 'commas':
+			return new Repeat(parse1(val),',',Number(key=='oneormore'));
+        case 'anynumber':
+        case 'oneormore':
+        	return new Repeat(parse1(val),' ',Number(key=='oneormore'));
+        case 'range2':
+        	return new Repeat(parse1(val['expr']),' ',val['min']['NUMBER'],val['max']['NUMBER']);
+        case 'range1':
+        	return new Repeat(parse1(val['expr']),' ',val['number']['NUMBER'],val['number']['NUMBER']);
+		case 'SYMBOL':
+			return new Symbol(val)
+		case 'VARIABLE':
+			return new Variable(val)
+	}
+
+}
+
+function random(val){
+	if(val_dict[val])
+		return val_dict[val][1]()
+	else
+		return val
+}
+function increaseVal(val,ind,str){
+	if(val_dict[val])
+		return val_dict[val][2](str,ind,1)
+	else
+		return str
+}
+function decreaseVal(val,ind,str){
+	if(val_dict[val])
+		return val_dict[val][2](str,ind,0)
+	else
+		return str
+}
+
+//errors
+function copy(){
+	return new this.constructor.apply(null,this.cons())
+}
+//errors
+function valCopy(){
+	return new this.constructor(this.children.map(function(x){
+		return x.valCopy();
+	}))
+}
+function changeState(state,undef){
+	resetProps()
+	if (state<this.min)state=this.min;
+	else if (this.max!=undef&&state>this.max)state=this.max;
+	this.state=state;
+}
+function increaseState(){
+	this.changeState(this.state+1);
+}
+function decreaseState(){
+	this.changeState(this.state-1);
+}
+
+function randomizeVars(){
+		this.children.forEach(function(x){x.randomizeVars();})
+		updateControls();
+};
+function table(){
+	if (Array.isArray(arguments[0]))arguments=arguments[0];
+	str=''
+	for (var i = 0; i < arguments.length; i++) {
+		str+=arguments[i]
+	};
+	return '<div class="controls-table">'+str+'</div>'
+}
+function row(){
+	if (Array.isArray(arguments[0]))arguments=arguments[0];
+	str=''
+	for (var i = 0; i < arguments.length; i++) {
+		str+=arguments[i]
+	};
+	return '<div class="controls-row">'+str+'</div>'
+}
+function Choose(children){
+	this.state=0;
+	this.min=0;
+	this.max=children.length-1
+	this.children=children;
+	this.render=function(){
+		return this.children[this.state].render();
+	}
+	this.show=function(arr,undef){
+		if(arr==undef){
+			var arr=[]
+			return[this.show(arr),arr]
+		}
+		var ind=arr.push(this)-1;
+		return table(row('<button onclick="curr_dict['+ind+'].decreaseState()">&lt;</button>',
+			'<input id="pla_'+ind+'" readonly="readonly" value="'+this.render()+'" onkeypress="pressed(event,this)"/>',
+			'<button onclick="getProps();curr_dict['+ind+'].randomizeVars()">R</button>',
+			'<button onclick="curr_dict['+ind+'].increaseState()">&gt;</button>'),
+			row(this.children[this.state].show(arr)))
+	}
+	this.copy=function(){
+		return new this.constructor(this.children.map(function(x){
+			return x.copy();
+		}))
+	}
+	this.valCopy=valCopy;
+	this._changeState=changeState;
+	this.changeState=function(state){
+		this._changeState(state);
+		updateControls();
+	}
+
+	this.increaseState=increaseState;
+	this.decreaseState=decreaseState;
+	this.randomizeVars=function(){
+		this.children[this.state].randomizeVars();
+		updateControls();
+	};
+}
+function Concatinate(children){
+	this.children=children;
+	this.render=function(){
+		return this._join(this.children.map(function(x){return x.render();}));
+	}
+	this.show=function(arr,undef){
+		if(arr==undef){
+			var arr=[]
+			return[this.show(arr),arr]
+		}
+		var ind=arr.push(this)-1;
+		return table(row('<input id="pla_'+ind+'" readonly="readonly" value="'+this.render()+'" onkeypress="pressed(event,this)"/>',
+			'<button onclick="getProps();curr_dict['+ind+'].randomizeVars()">R</button>'),
+			row(this.children.map(function(x){return x.show(arr);}),1))
+	}
+	this._join=function(arr){
+		return arr.join(' ')
+	}
+	this.copy=function(){
+		return new this.constructor(this.children.map(function(x){
+			return x.copy();
+		}))
+	}
+	this.valCopy=valCopy;
+	this.randomizeVars=randomizeVars;
+}
+function Repeat(child,sep,min,max){
+	self=this;
+	this.min=min;
+	console.log(min)
+	this.state=min;
+	this.max=max;
+	this.child=child;
+	this.children=[child];
+	if (this.state)loop(this.state-1).forEach(function(){self.children.push(self.child.copy())})
+	this.sep=sep
+	this.render=function(){
+		return this._join(this.children.map(function(x){return x.render();}));
+	}
+	this.show=function(arr,undef){
+		if(arr==undef){
+			var arr=[]
+			return[this.show(arr),arr]
+		}
+		var ind=arr.push(this)-1;
+		return table(row('<button onclick="curr_dict['+ind+'].decreaseState()">-</button>',
+			'<input id="pla_'+ind+'" readonly="readonly" value="'+this.render()+'" onkeypress="pressed(event,this)"/>',
+			'<button onclick="getProps();curr_dict['+ind+'].randomizeVars()">R</button>',
+			'<button onclick="curr_dict['+ind+'].increaseState()">+</button>'),
+			row(this.children.map(function(x){return x.show(arr);})))
+	}
+	this._join=function(arr){
+		return arr.join(this.sep)
+	}
+	this.copy=function(){
+		return new this.constructor(this.child.copy(),this.sep,this.min,this.max)
+	}
+	this.valCopy=valCopy;
+	this._changeState=changeState;
+	this.changeState=function(state){
+		self=this;
+		this._changeState(state);
+		var diff=this.children.length-this.state;
+		if (diff>0)loop(diff).forEach(function(){self.children.pop()})
+		else if (diff<0)loop(-diff).forEach(function(){self.children.push(self.child.copy())})
+		updateControls();
+	}
+	this.increaseState=increaseState;
+	this.decreaseState=decreaseState;
+	this.randomizeVars=randomizeVars;
+}
+function Check(child){
+	this.state=1;
+	this.min=0;
+	this.max=1;
+	this.child=child
+	this.children=[child];
+	this.render=function(){
+		if (this.state)
+	 		return this.children[0].render();
+	 	else
+	 		return ''
+	}
+	this.show=function(arr,undef){
+		if(arr==undef){
+			var arr=[]
+			return [this.show(arr),arr]
+		}
+		var ind=arr.push(this)-1;
+		return table(row('<button onclick="curr_dict['+ind+'].decreaseState()">OFF</button>',
+			'<input id="pla_'+ind+'" readonly="readonly" value="'+this.render()+'" onkeypress="pressed(event,this)"/>',
+			'<button onclick="getProps();curr_dict['+ind+'].randomizeVars()">R</button>',
+			'<button onclick="curr_dict['+ind+'].increaseState()">ON</button>'),
+			row((this.state?('<tr><td colspan=3>'+this.children[0].show(arr)+'</td></tr>'):'')))
+	}
+	this.copy=function(){
+		return new this.constructor(this.child.copy())
+	}
+	this.valCopy=valCopy;
+	this._changeState=changeState;
+	this.changeState=function(state){
+		self=this;
+		this._changeState(state);
+		var diff=this.children.length-this.state;
+		if (diff>0)loop(diff).forEach(function(){self.children.pop()})
+		else if (diff<0)loop(-diff).forEach(function(){self.children.push(self.child.copy())})
+		updateControls();
+	}
+	this.increaseState=increaseState;
+	this.decreaseState=decreaseState;
+	this.randomizeVars=randomizeVars;
+}
+function Variable(type,val,undef){
+	this.type=type;
+	this._random=function(){return random(this.type);};
+	this._increaseVal=function(ind){return increaseVal(this.type,ind,this.val);};
+	this._decreaseVal=function(ind){return decreaseVal(this.type,ind,this.val);};
+	if (val!=undef){
+		this.val=val;
+	}
+	else this.val=this._random();
+	this.randomizeVars=function(){
+		this.val=this._random();
+		updateControls();
+	};
+	this.increaseVal=function(ind){
+		this.val=this._increaseVal(ind);
+		updateControls();
+	};
+	this.decreaseVal=function(ind){
+		this.val=this._decreaseVal(ind);
+		updateControls();
+	};
+	this.render=function(){
+		return this.val;
+	};
+	this.show=function(arr,undef){
+		if(arr==undef){
+			var arr=[]
+			return[this.show(arr),arr]
+		}
+		var ind=arr.push(this)-1;
+		return row('<button onclick="curr_dict['+ind+'].decreaseVal('+ind+')">-</button>',
+			'<input id="pla_'+ind+'" class="val'+
+			((this.type=='color')?" color1":'')+
+			'" value="'+
+			this.render()+'" onchange="curr_dict['+
+			ind+'].val=this.value;updateControls();" onkeypress="pressed(event,this)" />',
+			'<button onclick="getProps();curr_dict['+ind+'].randomizeVars()">R</button>',
+			'<button onclick="curr_dict['+ind+'].increaseVal('+ind+')">+</button>');
+	}
+	this.changeVal=function(val,undef){
+		this.val=val;
+	};
+	this.copy=function(){
+		return new Variable(this.type);
+	};
+	this.valCopy=function(){
+		return new Variable(this.type,this.val);
+	};
+};
+var hash="#";
+function Symbol(str){
+	this.str=str;
+	this.render=function(){
+		return str;
+	}
+	this.show=function(arr,undef){
+		if(arr==undef){
+			var arr=[]
+			return[this.show(arr),arr]
+		}
+		return '<input readonly="readonly" class="sym" value="'+this.render()+'" /">';
+	}
+	this.valCopy=this.copy=function(){return new Symbol(this.str);}
+	this.randomizeVars=function(){};
+}
